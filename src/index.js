2c078503b056b8e1be92aecf46c6eca334693ec5
wehmoen <github@wehmoen.top>
2018-11-23 16:26:18 +0100

    adding source code

---
+++ src/index.js
@@ -0,0 +1,126 @@
+const fetch = require("node-fetch");
+
+OracleD = {
+    AccountAPI: {
+        API: class {
+            constructor(config) {
+                this.baseURL = 'https://api.oracle-d.com/v1';
+                this.api_key = config.api_key
+            }
+
+            async getAccountStatus(account) {
+                return new Promise((resolve, reject) => {
+                    if (account.constructor.name !== "Account") {
+                        throw new Error("account must be an instance of Account()")
+                    } else if (!account.hasOwnProperty("_id")) {
+                        throw new Error("account needs to be requested first")
+                    } else {
+                        fetch(this.baseURL + '/accounts/'+account._id, {
+                            method: 'get',
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'x-api-key': this.api_key,
+                            },
+                        })
+                            .then(res => res.json())
+                            .then(json => {
+                                if (json.code === 200) {
+                                    json.data = account.extend(json.data)
+                                    resolve(json);
+                                } else if (json.code >= 400) {
+                                    reject(json.errors[0])
+                                } else {
+                                    reject("An unknown error occurred. Please contact support@oracle-d.com")
+                                }
+                            }).catch(e => {
+                            reject(e);
+                        });
+                    }
+                })
+            }
+
+            async getAccountList() {
+                return new Promise((resolve, reject) => {
+                    fetch(this.baseURL + '/accounts', {
+                        method: 'get',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'x-api-key': this.api_key,
+                        },
+                    })
+                        .then(res => res.json())
+                        .then(json => {
+                            if (json.code === 200) {
+                                let accounts = [];
+                                json.data.forEach(a => {
+                                    let account = new OracleD.AccountAPI.Account(a);
+                                    accounts.push(account);
+                                });
+                                json.data = accounts;
+                                resolve(json);
+                            } else if (json.code >= 400) {
+                                reject(json.errors[0])
+                            } else {
+                                reject("An unknown error occurred. Please contact support@oracle-d.com")
+                            }
+                        }).catch(e => {
+                        reject(e);
+                    });
+                })
+            }
+
+            async requestAccount(account) {
+                return new Promise((resolve, reject) => {
+                    if (account.constructor.name !== "Account") {
+                        throw new Error("account must be an instance of Account()")
+                    } else if (account.hasOwnProperty("_id")) {
+                        throw new Error("account has already been requested")
+                    } else {
+                        fetch(this.baseURL + '/accounts/create', {
+                            method: 'post',
+                            body: JSON.stringify(account.toJSON()),
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'x-api-key': this.api_key,
+                            },
+                        })
+                            .then(res => res.json())
+                            .then(json => {
+                                if (json.code === 200) {
+                                    json.data = account.extend(json.data)
+                                    resolve(json);
+                                } else if (json.code >= 400) {
+                                    reject(json.errors[0])
+                                } else {
+                                    reject("An unknown error occured. Please contact support@oracle-d.com")
+                                }
+                            }).catch(e => {
+                            reject(e);
+                        });
+                    }
+                })
+            }
+        },
+        Account: class {
+            constructor(config) {
+                Object.keys(config).forEach(key => {
+                    this[key] = config[key];
+                });
+            }
+
+            extend(data) {
+                Object.keys(data).forEach(key => {
+                    this[key] = data[key];
+                });
+                return this;
+            }
+
+            toJSON() {
+                let {username, email} = this;
+                return {username, email}
+            }
+        }
+    }
+};
+
+module.exports = OracleD;
